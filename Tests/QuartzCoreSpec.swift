0c07689ef1750072c7997f3891e6de6945b95050
gumob <gumob.dev@gmail.com>
2019-07-03 12:06:46 +0900

    Add specs for extensions

---
+++ Tests/QuartzCoreSpec.swift
@@ -0,0 +1,9 @@
+//
+//  QuartzCoreSpec.swift
+//  Fluidable
+//
+//  Created by kojirof on 2019/07/03.
+//  Copyright © 2019 Gumob. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/VectorMathSpec.swift
@@ -0,0 +1,9 @@
+//
+//  VectorMathSpec.swift
+//  Fluidable
+//
+//  Created by kojirof on 2019/07/03.
+//  Copyright © 2019 Gumob. All rights reserved.
+//
+
+import Foundation
