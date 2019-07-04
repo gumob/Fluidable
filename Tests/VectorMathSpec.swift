2ccab712e5cfb49f8b0dba8f7d4c349e3bd63fca
gumob <gumob.dev@gmail.com>
2019-07-04 16:52:36 +0900

    Ignore VectorMath from unit test

--- Tests/VectorMathSpec.swift
+++ Tests/VectorMathSpec.swift
@@ -10,13 +10,18 @@ import Quick
 import Nimble
 @testable import Fluidable
 
-class VectorMathSpec: QuickSpec {
-    override func spec() {
-        describe("Foundation") {
-            describe("XXXXXXXX") {
-                it("XXXXXXXX") {
-                }
-            }
-        }
-    }
-}
+//class VectorMathSpec: QuickSpec {
+//    override func spec() {
+//        describe("VectorMath") {
+//            describe("Scalar") {
+//                it("Property") {
+//                    let scalar0: Scalar = 0.00000
+//                    let scalar1: Scalar = 0.00001
+//                    expect(scalar0 ~= scalar1).to(beTrue())
+//                    expect(Scalar(0.1).sign == Scalar(1)).to(beTrue())
+//                    expect(Scalar(-0.1).sign == Scalar(-1)).to(beTrue())
+//                }
+//            }
+//        }
+//    }
+//}
