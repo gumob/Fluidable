099065ac01e18825c44a592b302afcdfacc539c8
gumob <gumob.dev@gmail.com>
2019-07-09 17:18:33 +0900

    Cleanup

--- UITests/MainSpec+Transition.swift
+++ UITests/MainSpec+Transition.swift
@@ -28,18 +28,106 @@ extension MainSpec {
                 collectionView.swipe(from: CGVector(dx: 0.5, dy: 0.75), to: CGVector(dx: 0.5, dy: 0.25))
             }
         }
-        usleep(sec: 1.5)
+        usleep(sec: 1.2)
     }
 
     func dismissWithAnimation(app: XCUIApplication, model: RootModel) {
         let button: XCUIElement = app.buttons.element(matching: .button, identifier: model.overlayCloseButtonAccessibilityIdentifier)
         expect(button.isVisible).toEventually(beTrue(), timeout: 10)
         button.tap()
-        usleep(sec: 1.5)
+        usleep(sec: 1.2)
     }
 
     func dismissWithInteraction(app: XCUIApplication, model: RootModel) {
-        let option: (interact: XCUIElement?, target: XCUIElement?, direction: SwipeDirection) = {
+        let option: InteractiveDismissOption = self.getInteractiveDismissOption(app: app, model: model)
+        Logger()?.log("🧪", [
+            "option.interact".lpad() + String(describing: option.interact?.identifier),
+            "option.target".lpad() + String(describing: option.target?.identifier),
+            "option.direction".lpad() + String(describing: option.direction),
+        ])
+        /* NOTE: Check whether the view exists */
+        expect(option.interact?.exists ?? false).toEventually(beTrue(), timeout: 10)
+        /* NOTE: Scroll until scroll view reaches to bottom */
+        if let target: XCUIElement = option.target {
+            expect(target.exists).toEventually(beTrue(), timeout: 10)
+            option.interact?.swipe(to: target, avoid: [AvoidableElement.navigationBar, AvoidableElement.keyboard], from: app)
+        }
+        usleep(sec: 1.0)
+        /* NOTE: Perform dismiss interaction */
+        switch option.direction {
+        case .up: option.interact?.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
+        case .down: option.interact?.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .left: option.interact?.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
+        case .right: option.interact?.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
+        }
+        /* NOTE: Check whether the view controller already disappears */
+        let visibleView: XCUIElement = app.otherElements.element(matching: .other, identifier: model.visibleControllerViewAccessibilityIdentifier)
+        expect(visibleView.exists).toNotEventually(beTrue(), timeout: 10)
+    }
+
+    func dismissWithCancelInteraction(app: XCUIApplication, model: RootModel) {
+//        print(app.debugDescription)
+        let visibleView: XCUIElement = app.otherElements.element(matching: .other, identifier: model.visibleControllerViewAccessibilityIdentifier)
+        expect(visibleView.exists).toEventually(beTrue(), timeout: 10)
+        switch model {
+        case .navigationFluidModal:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .navigationFluidFullScreen:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .navigationDrawerTop:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
+        case .navigationDrawerBottom:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .navigationDrawerLeft:
+            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
+        case .navigationDrawerRight:
+            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
+        case .navigationSlideTop:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
+        case .navigationSlideBottom:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .navigationSlideLeft:
+            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
+        case .navigationSlideRight:
+            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
+        case .transitionFluidModal:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .transitionFluidFullScreen:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .transitionDrawerTop:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
+        case .transitionDrawerBottom:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .transitionDrawerLeft:
+            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
+        case .transitionDrawerRight:
+            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
+        case .transitionSlideTop:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
+        case .transitionSlideBottom:
+            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
+        case .transitionSlideLeft:
+            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
+        case .transitionSlideRight:
+            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
+        }
+        usleep(sec: 1.2)
+    }
+
+    func rotateAndRevertDevice(app: XCUIApplication, model: RootModel) {
+        XCUIDevice.shared.orientation = XCUIDevice.shared.orientation.isPortrait ? .landscapeLeft : .portrait
+        usleep(sec: 1.2)
+        XCUIDevice.shared.orientation = XCUIDevice.shared.orientation.isPortrait ? .landscapeLeft : .portrait
+        usleep(sec: 1.2)
+    }
+}
+
+extension MainSpec {
+
+    typealias InteractiveDismissOption = (interact: XCUIElement?, target: XCUIElement?, direction: SwipeDirection)
+
+    func getInteractiveDismissOption(app: XCUIApplication, model: RootModel) -> InteractiveDismissOption {
+        return {
             switch model {
             case .navigationFluidModal, .transitionFluidModal, .navigationFluidFullScreen, .transitionFluidFullScreen,
                  .navigationDrawerBottom, .transitionDrawerBottom, .navigationSlideBottom, .transitionSlideBottom:
@@ -153,84 +241,5 @@ extension MainSpec {
                 return (interact: interactView, target: nil, direction: direction)
             }
         }()
-
-        Logger()?.log("🧪", [
-            "option.interact".lpad() + String(describing: option.interact?.identifier),
-            "option.target".lpad() + String(describing: option.target?.identifier),
-            "option.direction".lpad() + String(describing: option.direction),
-        ])
-        expect(option.interact?.exists ?? false).toEventually(beTrue(), timeout: 10)
-        /* NOTE: Scroll until scroll view reaches to bottom */
-        if let target: XCUIElement = option.target {
-            expect(target.exists).toEventually(beTrue(), timeout: 10)
-            option.interact?.swipe(to: target, avoid: [AvoidableElement.navigationBar, AvoidableElement.keyboard], from: app)
-        }
-        usleep(sec: 1.0)
-        /* NOTE: Perform dismiss interaction */
-        switch option.direction {
-        case .up: option.interact?.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
-        case .down: option.interact?.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .left: option.interact?.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
-        case .right: option.interact?.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
-        }
-        /* NOTE: Check whether the view controller already disappears */
-        let visibleView: XCUIElement = app.otherElements.element(matching: .other, identifier: model.visibleControllerViewAccessibilityIdentifier)
-        expect(visibleView.exists).toNotEventually(beTrue(), timeout: 10)
-    }
-
-    func dismissWithCancelInteraction(app: XCUIApplication, model: RootModel) {
-//        print(app.debugDescription)
-        let visibleView: XCUIElement = app.otherElements.element(matching: .other, identifier: model.visibleControllerViewAccessibilityIdentifier)
-        expect(visibleView.exists).toEventually(beTrue(), timeout: 10)
-        switch model {
-        case .navigationFluidModal:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .navigationFluidFullScreen:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .navigationDrawerTop:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
-        case .navigationDrawerBottom:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .navigationDrawerLeft:
-            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
-        case .navigationDrawerRight:
-            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
-        case .navigationSlideTop:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
-        case .navigationSlideBottom:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .navigationSlideLeft:
-            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
-        case .navigationSlideRight:
-            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
-        case .transitionFluidModal:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .transitionFluidFullScreen:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .transitionDrawerTop:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
-        case .transitionDrawerBottom:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .transitionDrawerLeft:
-            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
-        case .transitionDrawerRight:
-            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
-        case .transitionSlideTop:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.8), to: CGVector(dx: 0.5, dy: 0.2))
-        case .transitionSlideBottom:
-            visibleView.swipe(from: CGVector(dx: 0.5, dy: 0.2), to: CGVector(dx: 0.5, dy: 0.8))
-        case .transitionSlideLeft:
-            visibleView.swipe(from: CGVector(dx: 0.8, dy: 0.5), to: CGVector(dx: 0.2, dy: 0.5))
-        case .transitionSlideRight:
-            visibleView.swipe(from: CGVector(dx: 0.2, dy: 0.5), to: CGVector(dx: 0.8, dy: 0.5))
-        }
-        usleep(sec: 1.5)
-    }
-
-    func rotateAndRevertDevice(app: XCUIApplication, model: RootModel) {
-        XCUIDevice.shared.orientation = XCUIDevice.shared.orientation.isPortrait ? .landscapeLeft : .portrait
-        usleep(sec: 1.5)
-        XCUIDevice.shared.orientation = XCUIDevice.shared.orientation.isPortrait ? .landscapeLeft : .portrait
-        usleep(sec: 1.5)
     }
 }
